57f0e4694f4ac845e2d44357c08dddd635105c59
DivyaDharshini012 <sridivyasvgithub@gmail.com>
2025-02-09 13:41:08 +0530

    changes

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -76,8 +76,9 @@ export const Main = () => {
                       <div>
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
-                        <img src={assets.send_icon} alt="" 
+                        {input ? <img src={assets.send_icon} alt="" 
                         onClick={()=>onSent()}/>
+                      : null}
                       </div>
                     </div>
                     <p className='bottom-info'>
